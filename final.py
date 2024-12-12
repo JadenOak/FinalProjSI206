7330eaa38fd94f8f16f091c935427916c8ccaba6
Jaden Oak <jadenoak@Jadens-MacBook-Air-2.local>
2024-12-11 23:14:36 -0500

    first commit

--- final.py
+++ final.py
@@ -3,3 +3,29 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+def fetch_weather_data(latitude, longitude):
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m,relative_humidity_2m,weather_code,wind_speed_10m&timezone=America%2FNew_York"
+    response = requests.get(url)
+    return response.json()
+
+wmo_weather_codes = {
+    0: "Clear sky",
+    1: "Mainly clear",
+    2: "Partly cloudy",
+    3: "Overcast",
+    45: "Fog",
+    51: "Light drizzle",
+    53: "Moderate drizzle",
+    55: "Dense drizzle",
+    61: "Light rain",
+    63: "Moderate rain",
+    65: "Heavy rain",
+    71: "Light snow",
+    73: "Moderate snow",
+    75: "Heavy snow",
+    80: "Rain showers",
+    81: "Moderate rain showers",
+    82: "Heavy rain showers",
+    85: "Snow showers",
+    95: "Thunderstorm",
+}
