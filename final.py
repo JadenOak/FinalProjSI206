ff2a3fce1138c19717241d148548e6886edab2c0
Jaden Oak <jadenoak@Jadens-MacBook-Air-2.local>
2024-12-11 23:22:34 -0500

    final commit

--- final.py
+++ final.py
@@ -56,3 +56,66 @@ for city, (lat, lon) in cities.items():
             "Weather Code": hourly["weather_code"][i]
         })
 
+df = pd.DataFrame(weather_data)
+df["Weather Description"] = df["Weather Code"].map(wmo_weather_codes)
+df["Weather Description"] = df["Weather Description"].fillna("Unknown")
+
+num_df = df.select_dtypes(include='number')
+avg_df = df.groupby("City")[num_df.columns].mean().reset_index()
+
+df = df.drop_duplicates(subset=['Date/Time', 'City'])
+
+# temperature trends
+sns.set(style="whitegrid")
+plt.figure(figsize=(10, 6))
+ax = sns.lineplot(data=df, x="Date/Time", y="Temperature (°C)", hue="City")
+
+unique = df["Date/Time"].unique()
+temp = unique[::12]
+ax.set_xticks(range(0, len(unique), 12))
+ax.set_xticklabels(temp, rotation=45)
+
+plt.title("Temperature (°C) Trends Over Time")
+plt.xlabel("Date/Time")
+plt.ylabel("Temperature (°C)")
+plt.tight_layout()
+plt.savefig("temperature_trends.png")
+plt.close()
+
+# average humidity
+plt.figure(figsize=(8, 6))
+sns.barplot(x="City", y="Humidity", data=avg_df)
+plt.title("Average Humidity by City")
+plt.xlabel("City")
+plt.ylabel("Humidity (%)")
+plt.savefig("average_humidity.png")
+plt.close()
+
+# heat map
+pivot_df = df.pivot(index="City", columns="Date/Time", values="Temperature (°C)")
+plt.figure(figsize=(12, 6))
+heatmap_ax = sns.heatmap(pivot_df, cmap="coolwarm", annot=False)
+heatmap_ax.set_xticks(range(0, len(pivot_df.columns), 12))
+heatmap_ax.set_xticklabels(pivot_df.columns[::12], rotation=45)
+
+plt.title("Temperature (°C) Comparison Over Time Between Cities")
+plt.xlabel("Date/Time")
+plt.ylabel("City")
+plt.tight_layout()
+plt.savefig("temperature_heatmap.png")
+plt.close()
+
+# weather description
+threshold = 0.05  # for 'other' category
+weather_freq = df["Weather Description"].value_counts()
+weather_freq = weather_freq / weather_freq.sum()
+other_freq = weather_freq[weather_freq < threshold].sum()
+weather_freq = weather_freq[weather_freq >= threshold]
+weather_freq["Other"] = other_freq
+
+plt.figure(figsize=(8, 6))
+weather_freq.plot.pie(autopct='%1.1f%%', startangle=90, labels=weather_freq.index)
+plt.title("Weather Description Frequency")
+plt.ylabel('')
+plt.savefig("weather_description_frequency.png")
+plt.close()
